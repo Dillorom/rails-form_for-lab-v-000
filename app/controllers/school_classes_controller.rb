052286b3af45f25ff1b96465f4f6f081388c0287
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-28 22:02:56 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,14 @@
 class SchoolClassesController < ApplicationController
+  def index
+    @school_classes = SchoolClass.all
+  end
+
+  def show 
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def create
+    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
 end
