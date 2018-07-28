297c67fdbc8e14a0fad400edff091b9f7277511d
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-28 21:13:13 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,10 @@ class StudentsController < ApplicationController
     @student.save
     redirect_to student_path(@student)
   end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student).permit(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
 end
